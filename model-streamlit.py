203c622d85a831243bde27ba0c943a39f89b3ff7
Veronica Leong <veronical1130@gmail.com>
2021-05-19 11:55:11 -0400

    updating notebooks + README

--- model-streamlit.py
+++ model-streamlit.py
@@ -3,18 +3,22 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
-from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 import pickle
 
 # load in data
-train_values = pd.read_csv('Proj5_train_values.csv')
-train_labels = pd.read_csv('Proj5_train_labels.csv')
+train_values = pd.read_csv('data/Proj5_train_values.csv')
+train_labels = pd.read_csv('data/Proj5_train_labels.csv')
+
+# Label Encode categorical features
+
+le = LabelEncoder()
+train_enc = train_values.apply(le.fit_transform)
+train_enc
 
 # merge train values + labels
 
-earthquake_encoded = pd.merge(train_values, train_labels, on = 'building_id')
-
+earthquake_encoded = pd.merge(train_enc, train_labels, on = 'building_id')
 
 # set X + y
 X = earthquake_encoded.drop(columns = ['building_id', 'damage_grade'])
@@ -25,23 +29,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, random_s
 
 # model
 
-pipe_forest = make_pipeline(OneHotEncoder(handle_unknown='ignore'), StandardScaler(with_mean=False), RandomForestClassifier(n_jobs = -1, max_depth = 11, max_features = 30))
-pipe_forest.fit(X_train, y_train)
+# scale
+sscaler = StandardScaler()
+X_train_scaled = sscaler.fit_transform(X_train)
+X_test_scaled = sscaler.transform(X_test)
 
-# ohe
-# ohe = OneHotEncoder(use_cat_names = True)
-# X_train_encoded = ohe.fit_transform(X_train)
-# X_test_encoded = ohe.transform(X_test)
-
-# # scale
-# sscaler = StandardScaler()
-# X_train_scaled = sscaler.fit_transform(X_train_encoded)
-# X_test_scaled = sscaler.transform(X_test_encoded)
-
-# # estimator
-# forest = RandomForestClassifier(max_depth = 11, max_features = 30)
-# forest.fit(X_train_scaled, y_train)
+# estimator
+forest = RandomForestClassifier(max_depth = 11, max_features = 30)
+forest.fit(X_train_scaled, y_train)
 
 # save model
 with open('saved-earthquake-model.pkl', 'wb') as model_file:
-    pickle.dump(pipe_forest, model_file)
+    pickle.dump(forest, model_file)
